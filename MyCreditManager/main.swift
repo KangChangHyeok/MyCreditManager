43efbece6ddd411131f56c1dee88a9acbf316c22
Kyle <dpkyle97@gmail.com>
2022-12-02 20:08:02 +0900

    성적추가(변경) 기능 추가

--- MyCreditManager/main.swift
+++ MyCreditManager/main.swift
@@ -11,7 +11,7 @@ var creditMangerExistence = true
 
 struct Student: Equatable {
     var name: String
-    var Subject: [String: String]?
+    var subject: [String: String]?
 }
 var students = [Student]()
 
@@ -28,12 +28,12 @@ while creditMangerExistence {
         deleteStudent()
         break
     case "3":
-        //성적 추가(변경) 기능 실행
+        addSubject()
         break
     case "4":
         // 성적 삭제 기능 실행
         break
-    case "5":
+    case "X":
         // 종료
         creditMangerExistence = false
     default:
@@ -64,9 +64,32 @@ func deleteStudent() {
             if let deleteStudentIndex = students.firstIndex(of: Student(name: input)) {
                 students.remove(at: deleteStudentIndex)
             }
-            
         }
-        print(students)
+    } else {
+        print("입력이 잘못되었습니다. 다시 확인해주세요")
+    }
+}
+func addSubject() {
+    print("성적을 추가할 학생의 이름, 과목 이름, 성적(A+, A, F 등)을 띄어쓰기로 구분하여 차례로 작성해주세요.")
+    print("입력예) Mickey Swift A+")
+    print("만약에 학생의 성적 중 해당 과목이 존재하면 기존 점수가 갱신됩니다.")
+    if let input = readLine()?.components(separatedBy: " "), input.count == 3 {
+        let name = input[0]
+        let subjectName = input[1]
+        let subjectScore = input[2]
+        
+        guard false == students.filter({ $0.name == name }).isEmpty else {
+            print("입력이 잘못되었습니다. 학생이 없음")
+            return
+        }
+        guard let addSubjectStudentIndex = students.firstIndex(where:{$0.name == name}) else { return }
+        
+        guard false == students[addSubjectStudentIndex].subject?.isEmpty else {
+            students[addSubjectStudentIndex].subject = [:]
+            students[addSubjectStudentIndex].subject?.updateValue(subjectScore, forKey: subjectName)
+            return
+        }
+        students[addSubjectStudentIndex].subject?.updateValue(subjectScore, forKey: subjectName)
     } else {
         print("입력이 잘못되었습니다. 다시 확인해주세요")
     }
